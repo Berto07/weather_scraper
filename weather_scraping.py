d41356da31ec024298b2083352ab09a3cd37f811
Berto07 <67025243+Berto07@users.noreply.github.com>
2022-01-10 21:14:38 -0500

    Second Commit

--- weather_scraping.py
+++ weather_scraping.py
@@ -1,22 +1,43 @@
 #scrape the weather.com website
 
-import requests
-import urllib
-from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
-import pprint
-import selenium
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-url = requests.get("https://weather.com/weather/today/l/7394a793971a4a7345c791564765f9b3ab107c83a0dfcc6d326b9684cac9c86d")
-url.raise_for_status()
-bs = soup(url.text, "html.parser")
+#Using Selenium without launching the browser (headless).
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver=webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
+search = input("Please enter your City or Zip Code: ")
+
+#Webdriver will navigate to weather.com, click the search bar, and enter the users input.
+url = driver.get('https://weather.com/')
+time.sleep(2)
+driver.find_element_by_id('LocationSearch_input').click()
+driver.find_element_by_id('LocationSearch_input').send_keys(search)
+time.sleep(2)
+driver.find_element_by_id('LocationSearch_input').send_keys(Keys.RETURN)
+time.sleep(2)
+print("")
+print("")
+
+#Access page content with Beautiful soup.
+page = driver.page_source
+bs = soup(page, "html.parser")
+
+time.sleep(3)
+
+#Soup will scape the page that Selenium navigates too
 city = bs.find("h1", {"class": "CurrentConditions--location--kyTeL"})
-pprint.pprint(city.get_text())
-time_of = bs.find("div", {"class": "CurrentConditions--timestamp--23dfw"})
-pprint.pprint(time_of.get_text())
-temperature = bs.find("span", {"class": "CurrentConditions--tempValue--3a50n"})
-print(temperature.get_text())
+print(city.text)
+tod = bs.find("span", {"class": "CurrentConditions--timestamp--23dfw"})
+print(tod.text)
+temp = bs.find("span", {"class": "CurrentConditions--tempValue--3a50n"})
+print(temp.text)
 
-
-
+    
+driver.close()
